976330eb0a38a95dbe2cdda39611d9b5f7fafc08
Guh-NubeliU <gustavo.barbosa@nubeliu.com>
2019-04-01 14:07:45 -0300

    automatic-push

--- shell/shellCompliance.sh
+++ shell/shellCompliance.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
- 
- DIRECTORIES="/tmp/complianceServer/ComplianceFiscal"
- MENSAGERIA="mensageria-etransparencia
- mensageria-ginfes"
- DIRLOG="csf-nfs/log/"
 
-for DIR in ${MENSAGERIA}; do 
-	find ${DIRECTORIES}/${DIR}/${DIRLOG} -iname '*.log.*' | xargs rm -rf
+DIRECTORIES="/tmp/complianceServer/ComplianceFiscal"
+DIRLOG="/csf-nfs/log/"
+
+for DIR in $(find ${DIRECTORIES} -iname 'mensageria-*'); do
+
+	cd ${DIR}${DIRLOG}
+	find ${DIR}${DIRLOG} -name '*.log.*' -exec tar -vrf compliance-timer-nfs-pref.log.tar.gz {} \;
+  mkdir old | mv compliance-timer-nfs-pref.log.tar.gz old/
+  find -maxdepth 1 -name '*.log.*' | xargs rm -rf 
 done
